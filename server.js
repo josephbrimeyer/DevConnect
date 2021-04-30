d8d8ad5ca874d3fe5729e7cd5b4b32939479efc5
Joe Brimeyer <jbrimeyer@gmail.com>
2021-04-29 21:04:43 -0700

    added and removed some comments.

--- server.js
+++ server.js
@@ -12,24 +12,12 @@ const app = express();
 process.env.API_KEY;
 process.env.DB_PASSWORD;
 
-// Requiring passport as we've configured it
-// const passport = require("passport");
-// const localStrategy = require("passport-local");
-// const passportLocalMongoose = require("passport-local-mongoose");
-// const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const methodOverride = require("method-override");
 const session = require("express-session");
-// let bodyParser = require("body-parser");
+let bodyParser = require("body-parser");
 
 const db = require("./models/index.js");
 const User = require("./models/User.js");
-// const session = require("express-session");
-let bodyParser = require("body-parser");
-
-//Linkedin Login using node js and passport
-// const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
-// const routes = require("./routes/api-routes");
-// const config = require("./config");
 
 // Middleware
 app.set("view engine", "ejs");
@@ -55,15 +43,17 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/users/auth/google/return",
+      callbackURL: "http://localhost:3001/users/auth/google/return",
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(profile);
+
       // passport callback function
       //check if user already exists in our db with the given profile ID
       User.findOne({ googleId: profile.id }).then((currentUser) => {
         if (currentUser) {
           console.log(profile);
+
           //if we already have a record with the given profile ID
           done(null, currentUser);
         } else {
@@ -101,47 +91,8 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-// passport.serializeUser(function (user, cb) {
-//   cb(null, user);
-// });
-
-// passport.deserializeUser(function (obj, cb) {
-//   cb(null, obj);
-// });
-
-// passport.use(
-//   new LinkedInStrategy(
-//     {
-//       clientID: config.linkedinAuth.clientID,
-//       clientSecret: config.linkedinAuth.clientSecret,
-//       callbackURL: config.linkedinAuth.callbackURL,
-//       scope: ["r_emailaddress", "r_liteprofile"],
-//     },
-//     function (token, tokenSecret, profile, done) {
-//       return done(null, profile);
-//     }
-//   )
-// );
-
-// // Define middleware here
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
 // Serve up static assets (usually on heroku)
 app.use(express.static("client/build"));
-
-// We need to use sessions to keep track of our user's login status
-// app.use(
-//   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// Define API routes here
-// require("./controllers/memberController.js");
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
 
 //homepage route
 app.get("/", (req, res) => {
@@ -151,17 +102,6 @@ app.get("/", (req, res) => {
 // routes
 const userRouter = require("./routes/users.js");
 app.use("/users", userRouter);
-// app.use("/", routes);
-
-// Requiring our routes
-// require("./routes/html-routes")(app);
-// require("./routes/api-routes")(app);
-
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server is now listening on port ${PORT}!`);
